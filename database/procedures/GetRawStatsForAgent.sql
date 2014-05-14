9af6c05fc5015995d693a771f00938a851a6346f
John <john@johnluetke.net>
2014-05-14 10:43:32 -0700

    Update GetRawStatsForAgent.sql
    Using latest timestamp as per day as key instead of highest value per day

--- database/procedures/GetRawStatsForAgent.sql
+++ database/procedures/GetRawStatsForAgent.sql
@@ -1,4 +1,4 @@
-CREATE DEFINER=`SRStats`@`localhost` PROCEDURE `GetRawStatsForAgent`(IN `agentName` VARCHAR(15))
+CREATE DEFINER=`admin`@`localhost` PROCEDURE `GetRawStatsForAgent`(IN `agentName` VARCHAR(15))
     READS SQL DATA
 BEGIN
 
@@ -7,14 +7,17 @@ DROP TABLE IF EXISTS RawStatsForAgent;
 SET @minDate = CURDATE();
 
 CREATE TEMPORARY TABLE TRawStatsForAgent
-	SELECT agent,
-	       CAST(timestamp as DATE) date,
-	       stat,
-	       MAX(value) value
-	  FROM Data 
-	 WHERE agent = agentName 
-  GROUP BY date, stat 
-  ORDER BY stat, date; 
+  SELECT agent, 
+         DATE(usable_timestamp) `date`, 
+         timestamp, 
+         stat, 
+         value
+    FROM Data
+   INNER JOIN (  SELECT MAX(timestamp)`usable_timestamp`
+                   FROM Data 
+	              WHERE agent = agentName 
+               GROUP BY DATE(timestamp)) q0
+         ON q0.`usable_timestamp` = Data.timestamp;
 
 SELECT MIN(date) FROM TRawStatsForAgent INTO @minDate;
 
@@ -22,6 +25,7 @@ CREATE TEMPORARY TABLE RawStatsForAgent
 	SELECT agent,
 	       (DATEDIFF(date, @minDate) + 1) timepoint,
 	       date,
+           timestamp,
 	       stat,
 	       MAX(value) value
 	  FROM TRawStatsForAgent 
