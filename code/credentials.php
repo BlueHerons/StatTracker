9c2e34519c309426cf8f4dae99e1e4769b4c665e
John Luetke <john@johnluetke.net>
2014-05-18 19:24:11 -0700

    Adding blank credentials.php

---
+++ code/credentials.php
@@ -0,0 +1,16 @@
+<?php
+define("DB_HOST", "");
+define("DB_NAME", "");
+define("DB_USER", "");
+define("DB_PASS", "");
+
+define("SMTP_HOST", "");
+define("SMTP_PORT", "");
+define("SMTP_ENCR", "");
+define("SMTP_USER", "");
+define("SMTP_PASS", "");
+
+define("GOOGLE_CLIENT_ID",     "");
+define("GOOGLE_CLIENT_SECRET", "");
+define("GOOGLE_APP_NAME",      "");
+?>
