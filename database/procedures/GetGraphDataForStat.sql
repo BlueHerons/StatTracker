03437d6a62b2e4c2799f3554957c5b0e929313a5
John <john@johnluetke.net>
2014-05-30 09:33:33 -0700

    Update GetGraphDataForStat.sql
    Utilize new Dates table

--- database/procedures/GetGraphDataForStat.sql
+++ database/procedures/GetGraphDataForStat.sql
@@ -3,9 +3,8 @@ CREATE DEFINER=`admin`@`localhost` PROCEDURE `GetGraphDataForStat`(IN `statName`
 BEGIN
 
 SET @minDate = CURDATE();
-SET @maxDate = CURDATE();
-SET @slope = 0;
 SET @intercept = 0;
+SET @slope = 0;
 SET @agent = '';
 
 SELECT DISTINCT agent FROM RawStatsForAgent LIMIT 1 INTO @agent;
@@ -19,14 +18,13 @@ SELECT MAX(`date`) FROM RawStatForAgent INTO @maxDate;
 SELECT slope FROM BadgePrediction INTO @slope;
 SELECT intercept FROM BadgePrediction INTO @intercept;
 
-CALL CreateDatelist(@minDate, @maxDate);
-
 CREATE TEMPORARY TABLE GraphDataForStat
     SELECT dl.date `Date`,
 	       r.value `Actual`,
            CEIL(@intercept + (@slope * (DATEDIFF(dl.date, @minDate) + 1))) `Regression`
       FROM RawStatForAgent r 
-RIGHT JOIN Datelist dl 
-           ON dl.date = r.date;
+RIGHT JOIN Dates dl 
+           ON dl.date = r.date
+WHERE dl.date <= @maxDate;
 
 END
