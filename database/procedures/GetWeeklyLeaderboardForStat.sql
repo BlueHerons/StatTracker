c36ef99f34ed9a4b5d114688c9313142bbf23833
John Luetke <john@johnluetke.net>
2014-06-18 22:15:23 -0700

    Tweaking weekly leaderboard calculations. See #7.

--- database/procedures/GetWeeklyLeaderboardForStat.sql
+++ database/procedures/GetWeeklyLeaderboardForStat.sql
@@ -7,52 +7,58 @@ DECLARE done INT DEFAULT false;
 DECLARE agent_cursor CURSOR FOR SELECT agent FROM weekly;
 DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = true;
 
+SET @debug_enabled = TRUE;
+
 DROP TABLE IF EXISTS `LeaderboardForStat`;
 DROP TABLE IF EXISTS `weekly`;
 
 CREATE TEMPORARY TABLE weekly (
   `agent` VARCHAR(20) NOT NULL,
-  `actual_value` INT(11) NOT NULL,
-  `regression` BIT(1) NOT NULL,
-  `rate` DOUBLE(10,2) NOT NULL,
-  `start_timepoint` INT(4) NOT NULL,
-  `latest_timepoint` INT(4) NOT NULL,
-  `latest_date` DATE NOT NULL,
-  `delta_timepoint` INT(4) NOT NULL,
-  `start_value` INT(11) NOT NULL,
   `latest_value` INT(11) NOT NULL,
-  `delta_value` INT(11) NOT NULL
+  `latest_date` DATE NOT NULL,
+  `latest_timepoint` INT(4) NOT NULL,
+  `actual_value` INT(11) NOT NULL,
+  `start_timepoint` INT(4) NOT NULL,
+  `delta_timepoint` INT(4) NOT NULL,
+  `rate` DOUBLE(10,2) NOT NULL,
+  `start_value` INT(11) NOT NULL,  
+  `delta_value` INT(11) NOT NULL,
+  primary key(agent)
 );
 
-# Insert all agents that have submitted this week
+# Insert all agents that have submitted since the start date
 # If no submission exists for the week start, flag by setting regression = 1
-INSERT INTO weekly (agent, actual_value, regression)
-SELECT agent,
-       IF(CAST(MIN(timestamp) AS Date) != start_date, null, value) `actual_value`,
-       IF(CAST(MIN(timestamp) AS Date) != start_date, 1, 0) `regression`
+INSERT INTO weekly (agent)
+SELECT agent
   FROM `Data`
  WHERE timestamp >= start_date AND
        timestamp <= DATE_ADD(start_date, INTERVAL 7 DAY) AND
        stat = stat_name
 GROUP BY agent;
 
-# For all rows where regression = 1, we will caluclate a value based on the agent's regression slope
-UPDATE weekly SET start_timepoint = GetTimepointForAgentAndDate(agent, start_date) WHERE regression = 1;
-UPDATE weekly SET rate = GetRateForAgentAndStat(agent, stat_name) WHERE regression = 1;
-UPDATE weekly SET start_value = actual_value WHERE regression = 0;
-
-# Delete any agent for which we cannot calculate a weekly value or regression
-#DELETE FROM weekly WHERE start_value = 0;
+# For all rows where regression = 1, we will calculate a value based on the agent's regression slope
+#UPDATE weekly SET start_timepoint = GetTimepointForAgentAndDate(agent, DATE_SUB(start_date, INTERVAL 1 DAY));
+#UPDATE weekly SET rate = GetRateForAgentAndStat(agent, stat_name);
 
 # Find the latest submission from each agent this week
+SET @actual_value = 0;
+SET @start_value = 0;
 SET @latest_value = 0;
 SET @latest_timestamp = 0;
 OPEN agent_cursor;
 
 agent_loop: LOOP
     FETCH agent_cursor INTO agent_name;
+
+	#CALL debug(@debug_enabled, (SELECT agent_name));
+	#CALL debug(@debug_enabled, (SELECT done));
+
     IF done THEN LEAVE agent_loop; END IF;
 
+	UPDATE weekly SET start_timepoint = GetTimepointForAgentAndDate(agent_name, DATE_SUB(start_date, INTERVAL 1 DAY)) WHERE agent = agent_name;
+	UPDATE weekly SET rate = GetRateForAgentAndStat(agent_name, stat_name) WHERE agent = agent_name;
+
+	# Get latest submitted value
     SELECT value, timestamp
       FROM (SELECT timestamp,
 				   value
@@ -63,11 +69,45 @@ agent_loop: LOOP
 				   stat = stat_name
 		  ORDER BY timestamp DESC) q1 LIMIT 1 INTO @latest_value, @latest_timestamp;
 
-    UPDATE weekly SET latest_date = DATE(@latest_timestamp) WHERE agent = agent_name;
-    UPDATE weekly SET latest_value = @latest_value, latest_timepoint = GetTimepointForAgentAndDate(agent_name, DATE(@latest_timestamp)) WHERE agent = agent_name;
-    UPDATE weekly SET delta_timepoint = latest_timepoint - start_timepoint WHERE agent = agent_name AND start_value = 0;
-    UPDATE weekly SET start_value = @latest_value - (delta_timepoint * rate) WHERE agent = agent_name AND start_value = 0;
-    UPDATE weekly SET delta_value = (@latest_value - start_value) WHERE agent = agent_name;
+	UPDATE weekly 
+	   SET latest_date = DATE(@latest_timestamp),
+	       latest_timepoint = GetTimepointForAgentAndDate(agent_name, DATE(@latest_timestamp)),
+	       latest_value = @latest_value
+	 WHERE agent = agent_name;
+
+	# start_value = value from start_date - 1, if exists (actual value)
+	  SELECT count(*)
+	    FROM `Data`
+	   WHERE DATE(timestamp) = DATE_SUB(start_date, INTERVAL 1 DAY) AND
+	         stat = stat_name AND
+	         agent = agent_name
+	ORDER BY timestamp DESC LIMIT 1 INTO @actual_value;
+
+	IF @actual_value > 0 THEN
+		  SELECT value
+			FROM `Data`
+		   WHERE DATE(timestamp) = DATE_SUB(start_date, INTERVAL 1 DAY) AND
+				 stat = stat_name AND
+				 agent = agent_name
+		ORDER BY timestamp DESC LIMIT 1 INTO @actual_value;
+
+		UPDATE weekly 
+		   SET actual_value = @actual_value, 
+		       start_value = @actual_value 
+		 WHERE agent = agent_name;
+	ELSE
+		# start value = regression on start_date - 1, if above false
+		UPDATE weekly 
+		   SET delta_timepoint = latest_timepoint - start_timepoint,
+		       start_value = latest_value - (delta_timepoint * rate), 
+		       actual_value = -1
+		 WHERE agent = agent_name;
+	END IF;
+
+	UPDATE weekly
+	   SET delta_value = latest_value - start_value
+	 WHERE agent = agent_name;
+
 END LOOP;
 
 # Delete any agent for which we cannot calculate a weekly value or regression
@@ -82,4 +122,4 @@ CREATE TEMPORARY TABLE LeaderboardForStat
          (SELECT @rank := 0) r 
 ORDER BY delta_value DESC;
 
-END;
+END
