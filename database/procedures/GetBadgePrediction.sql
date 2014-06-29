55218bf93443f9cc7671ffea45953554bcf16e4b
John Luetke <john@johnluetke.net>
2014-06-28 22:58:09 -0700

    Tweaking calculation of time remaining for Guardian

--- database/procedures/GetBadgePrediction.sql
+++ database/procedures/GetBadgePrediction.sql
@@ -1,4 +1,4 @@
-CREATE DEFINER=`SRStats`@`localhost` PROCEDURE `GetBadgePrediction`(IN `stat_key` VARCHAR(20))
+CREATE DEFINER=`admin`@`localhost` PROCEDURE `GetBadgePrediction`(IN `stat_key` VARCHAR(20))
     READS SQL DATA
 BEGIN
 
@@ -13,11 +13,6 @@ SET @sumXY = 0;
 SET @y = 0;
 SET @slope = 0;
 SET @intercept = 0;
-
-SET @silver_remaining = 0;
-SET @gold_remaining = 0;
-SET @platinum_remaining = 0;
-SET @onyx_remaining = 0;
 
 SET @max = 0;
 SET @stat = '';
@@ -39,7 +34,7 @@ SELECT (((@n * @sumXY) - (@sumX * @sumY)) / ((@n * @sumX2) - (@sumX * @sumX))) I
 SELECT ((@sumY - (@slope * @sumX)) / @n) INTO @intercept;
 SELECT @slope INTO @rate;
 
-SELECT MAX(value) FROM RawStatsForAgent WHERE stat = stat_key INTO @max;
+SELECT value FROM RawStatsForAgent WHERE stat = stat_key ORDER BY timestamp DESC LIMIT 1 INTO @max;
 
 SELECT name FROM Stats WHERE stat = stat_key INTO @stat;
 SELECT unit FROM Stats WHERE stat = stat_key INTO @unit;
@@ -51,54 +46,8 @@ SELECT amount_required - @max FROM Badges WHERE stat = stat_key AND level = @nex
 SELECT amount_required FROM Badges WHERE stat = stat_key AND level = @next INTO @y;
 SELECT @remaining / @slope INTO @days;
 
-IF stat_key = 'ap' THEN
-	SET @ap_required = 0;
-	SET @silver_obtained = 0;
-	SET @gold_obtained = 0;
-	SET @platinum_obtained = 0;
-	SET @onyx_obtained = 0;
-
-	CALL GetBadgeCount();
-
-	SELECT `count` FROM BadgeCount WHERE level = 'silver' INTO @silver_obtained;
-	SELECT `count` FROM BadgeCount WHERE level = 'gold' INTO @gold_obtained;
-	SELECT `count` FROM BadgeCount WHERE level = 'platinum' INTO @platinum_obtained;
-	SELECT `count` FROM BadgeCount WHERE level = 'onyx' INTO @onyx_obtained;
-
-	  SELECT `level` 
-        FROM Level 
-       WHERE ap_required <= @max AND 
-             silver_required <= @silver_obtained AND 
-             gold_required <= @gold_obtained AND 
-             platinum_required <= @platinum_obtained AND 
-             onyx_required <= @onyx_obtained 
-    ORDER BY level DESC 
-       LIMIT 1 INTO @current;
-
-	SELECT `level` 
-      FROM Level 
-     WHERE level = @current + 1 INTO @next;
-
-	SELECT `ap_required` 
-      FROM Level 
-     WHERE level = @current INTO @ap_required;
-
-	SELECT silver_required - @silver_obtained FROM Level WHERE level = @next INTO @silver_remaining;
-	SELECT gold_required - @gold_obtained FROM Level WHERE level = @next INTO @gold_remaining;
-	SELECT platinum_required - @platinum_obtained FROM Level WHERE level = @next INTO @platinum_remaining;
-	SELECT onyx_required - @onyx_obtained FROM Level WHERE level = @next INTO @onyx_remaining;
-
-	SELECT IF(@silver_remaining < 0, 0, @silver_remaining) INTO @silver_remaining;
-	SELECT IF(@gold_remaining < 0, 0, @gold_remaining) INTO @gold_remaining;
-	SELECT IF(@platinum_remaining < 0, 0, @platinum_remaining) INTO @platinum_remaining;
-	SELECT IF(@onyx_remaining < 0, 0, @onyx_remaining) INTO @onyx_remaining;
-	
-	IF @ap_required <= @max THEN
-		SET @remaining = 0;
-		SET @y = @max;
-		SET @days = 0;
-	END IF;
-
+IF stat_key = 'oldest_portal' THEN
+	SELECT @remaining INTO @days;
 END IF;
 
 IF @days < 0 THEN
@@ -113,13 +62,10 @@ CREATE TEMPORARY TABLE BadgePrediction
 	       @current `Current`,
 	       @next `Next`,
 	       ROUND((@max / @y), 2) `progress`,
+	       @max `obtained`,
 	       @remaining `Remaining`,
-	       @silver_remaining `silver_remaining`,
-	       @gold_remaining `gold_remaining`,
-	       @platinum_remaining `platinum_remaining`,
-	       @onyx_remaining `onyx_remaining`,
 	       ROUND(@days, 1) `Days`,
-	       ROUND(@rate, 1) `Rate`,
+	       ROUND(@rate, 0) `Rate`,
 	       ROUND(@intercept) `intercept`,
 	       ROUND(@slope, 2) `slope`;
 
