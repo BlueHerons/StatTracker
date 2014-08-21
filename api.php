fb6d64b545ff7fb3a340d31bde5ca207feafeab9
John Luetke <john@johnluetke.net>
2014-08-21 11:49:16 -0700

    Adding new endpoint for badges

--- api.php
+++ api.php
@@ -5,7 +5,7 @@ require_once("code/Agent.class.php");
 require_once("code/Authentication.class.php");
 require_once("vendor/autoload.php");
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 $mysql = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if ($mysql->connect_errno) {
@@ -14,9 +14,26 @@ if ($mysql->connect_errno) {
 
 $app = new Silex\Application();
 
+// Retrieve basic information about the agent
 $app->get("/api/{auth_code}", function($auth_code) use ($app) {
 	return $app->json(Agent::lookupAgentByAuthCode($auth_code));
 });
+
+// Retrieve badge information for the agent
+$app->get("/api/{auth_code}/badges/{what}", function(Request $request, $auth_code, $what) use ($app) {
+	$agent = Agent::lookupAgentByAuthCode($auth_code);
+	$limit = is_numeric($request->query->get("limit")) ? (int)$request->query->get("limit") : 4;
+
+	switch ($what) {
+		case "upcoming":
+			$data = $agent->getUpcomingBadges($limit);
+			break;
+		case "current":
+			$data = $agent->getBadges();
+			break;
+	}
+	return $app->json($data);
+})->value("what", "current");
 
 $app->run();
 ?>
