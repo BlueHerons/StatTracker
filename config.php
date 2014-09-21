0b00f2921c92ad8077c7bb4291dbb8923f26702e
John Luetke <john@johnluetke.net>
2014-09-20 19:42:12 -0700

    Adding faction colors

--- config.php
+++ config.php
@@ -15,6 +15,9 @@ define("GOOGLE_CLIENT_SECRET", "");
 define("GOOGLE_APP_NAME",      "");
 define("GOOGLE_REDIRECT_URL",  "");
 
+define("RES_BLUE",  "#00BFFF");
+define("ENL_GREEN", "#2BED1B");
+
 define("GROUP_NAME",  "The Local Resistance");
 define("GROUP_EMAIL", "stats@localresistance.com");
 define("ADMIN_AGENT", "YourIngressAgentName");
