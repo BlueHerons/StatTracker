9679bcb0e391a256e5c5ebc0a5a474ea9e511fb6
John Luetke <john@johnluetke.net>
2014-10-14 08:40:46 -0700

    Working out OCR recognition.
    Converted ocr.php to a test page, which main processing in OCR.class.php

---
+++ code/OCR.class.php
@@ -0,0 +1,164 @@
+<?php
+
+class OCR {
+
+	const MIN_IMAGE_WIDTH = 600;
+	const LEVENSHTEIN_THRESHOLD = 10;
+
+	public static function scanAgentProfile($imagePath) {
+		$imagePath = self::prepareImage($imagePath);
+		$lines = self::executeOCR($imagePath);
+		$lines = self::filterOCRResults($lines);
+		$data = self::processAgentData($lines);
+		return $data;
+	}
+
+	public static function getTempFileName() {
+		return substr(str_shuffle(md5(time())), 0, 6);
+	}
+
+	/**
+	 * Prepares an image for OCR
+	 *
+	 * @param
+	 *
+	 * @return path to image for OCR use
+	 */ 
+	public static function prepareImage($imagePath) {
+		// TODO: define path for temp files
+		$newWidth = 600;
+		$contrast = 2;
+		$newFile = self::getTempFileName() . ".png";
+		$img = new Imagick($imagePath);
+
+		if ($img->getImageWidth() < $newWidth) {
+			$newHeight = (int)(($newWidth * $img->getImageHeight()) / $img->getImageWidth());
+			$img->resizeImage($newWidth, $newHeight, Imagick::FILTER_LANCZOS, 1);
+		}
+
+		do {
+			$img->contrastImage(true);
+		} while (--$contrast > 0);
+
+		$img->writeImage("uploads/" . $newFile);
+		return "uploads/" . $newFile;
+
+	}
+	
+	public static function executeOCR($imagePath) {
+		$tesseract = new TesseractOCR($imagePath);
+		//$tesseract->setLanguage("eng");
+		$tesseract->setWhitelist(range('A','Z'), range('a','z'), range('0','9'), ',', '-', '<');
+		$lines = array_values(array_filter(explode("\n", $tesseract->recognize())));
+		$lines = array_filter($lines, function($item) {
+			return strlen(trim($item)) > 0;
+		});
+
+		return $lines;	
+	}
+
+	public static function filterOCRResults($lines) {
+		// Remove lines less than 1 char
+		$lines = array_filter($lines, function($item) {
+			return strlen(trim($item)) > 1;
+		});
+
+		$lines = array_filter($lines, function($item) {	
+			return strpos(trim($item), "LVL") !== 0;
+		});
+
+		return array_values($lines);
+	}
+
+	public static function processAgentData($lines) {
+		$agent = preg_grep("/ <$/", $lines);
+		$agent = array_values($agent);
+		if (sizeof($agent) != 1) { 
+			echo "error detecting agent, not 1 result: ";
+			print_r($agent);
+			//die();
+		}
+		$res = preg_match("/^([A-Za-z0-9]{3,15}) .*<$/", $agent[0], $matches);
+		$agent = $matches[1];
+
+		$ap = preg_grep("/ AP$/", $lines);
+		$ap = array_values($ap);
+		if (sizeof($ap) != 1) { 
+			echo "error detecting ap, not 1 result: ";
+			print_r($ap);
+			//die();
+		}
+		$res = preg_match("/([0-9,]{1,10})\/", $ap[0], $matches);
+		$ap = preg_replace("/,/", "", $matches[1]);
+
+		$stats = array();
+
+		foreach(StatTracker::getStats() as $stat) {
+			$stats[$stat->stat] = self::findAgentStatValue($stat->name, $stat->unit, $lines);
+		}
+
+		// Add in AP, since it requires special processing
+		if (empty($stats['ap'])) {
+			//$stats['ap'] = $ap;
+		}
+
+		print_r(array("agent" => $agent, "stats" => $stats));
+	}
+
+	private static function findAgentStatValue($stat_name, $unit, &$lines) {
+		$score = array();
+		$index = -1;
+		print_r($lines);
+
+		foreach ($lines as $line) {
+			$index++;
+			// Skip lines that clearly aren't a match
+			if (strlen($stat_name) >= strlen($line))
+				continue;
+
+			// Strip numerics from the line for comparison
+			$name_line = trim(preg_replace("/[0-9,]/", "", $line));
+			$value_line = trim(str_replace(explode(" ", $stat_name), "", $line));
+
+			$s = array (
+				"index" => $index,
+				"name" => $stat_name,
+				"line" => $line,
+				"value_line" => $value_line,
+				"name_line" => $name_line,
+				"score" => levenshtein($stat_name, $name_line),
+				"value" => trim(preg_replace("/[^0-9lt]/", "", $value_line)) // 1 is sometimes read as l or t
+			);
+
+			// If every character needs to be replaced, clearly not a match
+			if (strlen($name_line) == $s['score'] || $s['score'] > self::LEVENSHTEIN_THRESHOLD)
+				continue;
+
+			$s['final_value'] = $s['value'];
+
+			// replace l with 1
+			$s['value'] = str_replace("l", "1", $s['value']);
+			$s['value'] = str_replace("t", "1", $s['value']);
+
+			$score[] = $s;
+		}
+
+		usort($score, function($a, $b) {
+			if ($a["score"] == $b["score"]) return 0;
+			return ($a["score"] < $b["score"]) ? -1 : 1;
+		});
+
+		// "Best match" should be first entry
+		$line = array_shift($score);
+		array_splice($lines, $line['index'], 1);
+
+		print_r($line);
+
+		if ($stat_name == "AP") {
+			$line['value'] = preg_replace("/[^0-9]/", "", explode(" ", $line['value_line'])[0]);
+		}
+
+		return $line['value'];
+	}
+}
+?>
--- ocr.php
+++ ocr.php
@@ -1,38 +1,28 @@
 <?php
-
+require_once("config.php");
+require_once("code/StatTracker.class.php");
+require_once("code/OCR.class.php");
 require_once("vendor/autoload.php");
 
-$tesseract = new TesseractOCR('uploads/720x1923.png.214e73a0ed000131a42c22000b2204cd');
-$tesseract->setWhitelist(range('A','Z'), range('a','z'), range('0','9'), ',', '-', '<'); //tesseract will threat everything as downcase letters
-$lines = array_values(array_filter(explode("\n", $tesseract->recognize())));
-print_r($lines);
-
-$agent = preg_grep("/ <$/", $lines);
-$agent = array_values($agent);
-if (sizeof($agent) != 1) { 
-	echo "error detecting agent, not 1 result: ";
-	print_r($agent);
-	die();
+//print_r($_FILES);
+$mysql = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if ($mysql->connect_errno) {
+	die(sprintf("%s: %s", $mysql->connect_errno, $mysql->connect_error));
 }
 
-$res = preg_match("/^([A-Za-z0-9]{3,15}) .*<$/", $agent[0], $matches);
-$agent = $matches[1];
-
-$ap = preg_grep("/ AP$/", $lines);
-$ap = array_values($ap);
-if (sizeof($ap) != 1) { 
-	echo "error detecting ap, not 1 result: ";
-	print_r($ap);
-	die();
+if (!empty($_FILES['screenshot'])) {
+	$file = "uploads/" . OCR::getTempFileName() . ".png";
+	move_uploaded_file($_FILES['screenshot']['tmp_name'], $file);
+	$file = OCR::prepareImage($file);
+	?><img src="<?php echo $file;?>" /><?php
+	?><pre><?php
+	$data = OCR::scanAgentProfile($file);
+	print_r($data);
+	?></pre><?php
 }
+?>
 
-$res = preg_match("/([0-9,]{1,10})/", $ap[0], $matches);
-$ap = preg_replace("/,/", "", $matches[1]);
-
-
-find_stat("Unique Portals Visited", "", $lines);
-
-function find_stat($name, $regex, $lines) {
-	$line = preg_grep("/^{$name}/", $lines);
-	var_dump($line);
-}
+<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post" enctype="multipart/form-data">
+	<input type="file" name="screenshot" />
+	<input type="submit" />
+</form>
