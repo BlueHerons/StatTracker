e7c89f68d1cd5565c607799beebd27667c1434f3
John Luetke <john@johnluetke.net>
2014-10-17 23:47:40 -0700

    Tweaking daily trend

--- database/procedures/GetDailyTrend.sql
+++ database/procedures/GetDailyTrend.sql
@@ -3,19 +3,18 @@ CREATE DEFINER=`jluetke`@`localhost` PROCEDURE `GetDailyTrend`(IN `agent_name` V
     NO SQL
 BEGIN
 
-SELECT GetRateForAgentAndStatBetweenDates(agent_name, stat_key, start_date, end_date) INTO @rate;
-SELECT GetInterceptForAgentAndStatBetweenDates(agent_name, stat_key, start_date, end_date) INTO @intercept;
+SELECT GetRateForAgentAndStatBetweenDates(agent_name, stat_key, DATE_SUB(start_date, INTERVAL 30 day), start_date) INTO @rate;
 SELECT GetValueForAgentAndStatOnDate(agent_name, stat_key, DATE_SUB(start_date, INTERVAL 1 DAY)) INTO @start;
 
 CREATE TEMPORARY TABLE DailyTrend AS
 SELECT date,
-       IF (date > NOW(), NULL, COALESCE(value - @lastValue, 0)) `value`,
        @rate `target`,
-       @lastValue := value
+       IF (date > NOW(), NULL, COALESCE(value - @lastValue, (@lastValue + @rate) - @lastValue)) `value`,
+       @lastValue := COALESCE(value, @lastValue + @rate)
   FROM (SELECT @lastValue := @start) vars,
-    (SELECT dl.date `date`, 
+(SELECT dl.date `date`, 
            @start `start`,
-           COALESCE(q.value, ((GetTimepointForAgentAndDate(agent_name, dl.date) * @rate) + @intercept)) `value`
+           q.value `value`
       FROM Dates dl
  LEFT JOIN (SELECT date, value
               FROM Data 
