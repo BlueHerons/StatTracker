233529ddde3cccfa937799ffda65e9717a374470
John Luetke <john@johnluetke.net>
2014-10-23 15:29:47 -0700

    Major refactor of OCR class after consulting from ingress-stats.org

--- code/OCR.class.php
+++ code/OCR.class.php
@@ -2,172 +2,262 @@
 
 class OCR {
 
-	const MIN_IMAGE_WIDTH = 600;
-	const LEVENSHTEIN_THRESHOLD = 10;
-
+	/**
+	 * Scans an agent profile screenshot and returns an array of stat values.
+	 *
+	 * @param string $imagePath the path to the image
+	 *
+	 * @return array array of stat key's and values from the screenshot
+	 */
 	public static function scanAgentProfile($imagePath) {
-		$imagePath = self::prepareImage($imagePath);
+		$imagePath = self::convertToPBM($imagePath);
 		$lines = self::executeOCR($imagePath);
-		$lines = self::filterOCRResults($lines);
 		$data = self::processAgentData($lines);
 		return $data;
 	}
 
-	public static function getTempFileName() {
-		return substr(str_shuffle(md5(time())), 0, 6);
+	/**
+	 * Generates a random, temporary file name.
+	 *
+	 * @param string $extension optional extension for the filename
+	 *
+	 * @return temporary file name
+	 */
+	public static function getTempFileName($extension = "png") {
+		return substr(str_shuffle(md5(time())), 0, 6) . "." . $extension;
 	}
 
 	/**
-	 * Prepares an image for OCR
+	 * Converts a source image to PBM format for use with the OCRAD scanner. Also blacks out agent avatar and
+	 * stat labels.
 	 *
-	 * @param
+	 * Special thanks to Eu Ji (https://plus.google.com/115412883382513227875/about) of ingress-stats.org
 	 *
-	 * @return path to image for OCR use
-	 */ 
-	public static function prepareImage($imagePath) {
-		// TODO: define path for temp files
-		$newWidth = 600;
-		$contrast = 2;
-		$newFile = self::getTempFileName() . ".png";
-		$img = new Imagick($imagePath);
-
-		if ($img->getImageWidth() < $newWidth) {
-			$newHeight = (int)(($newWidth * $img->getImageHeight()) / $img->getImageWidth());
-			$img->resizeImage($newWidth, $newHeight, Imagick::FILTER_LANCZOS, 1);
-		}
-
-		do {
-			$img->contrastImage(true);
-		} while (--$contrast > 0);
-
-		$img->writeImage("uploads/" . $newFile);
-		return "uploads/" . $newFile;
-
-	}
-	
-	public static function executeOCR($imagePath) {
-		$tesseract = new TesseractOCR($imagePath);
-		//$tesseract->setLanguage("eng");
-		$tesseract->setWhitelist(range('A','Z'), range('a','z'), range('0','9'), ',', '-', '<');
-		$lines = array_values(array_filter(explode("\n", $tesseract->recognize())));
-		$lines = array_filter($lines, function($item) {
-			return strlen(trim($item)) > 0;
-		});
-
-		return $lines;	
-	}
-
-	public static function filterOCRResults($lines) {
-		// Remove lines less than 1 char
-		$lines = array_filter($lines, function($item) {
-			return strlen(trim($item)) > 1;
-		});
-
-		$lines = array_filter($lines, function($item) {	
-			return strpos(trim($item), "LVL") !== 0;
-		});
-
-		return array_values($lines);
-	}
-
-	public static function processAgentData($lines) {
-		$agent = preg_grep("/ <$/", $lines);
-		$agent = array_values($agent);
-		if (sizeof($agent) != 1) { 
-			echo "error detecting agent, not 1 result: ";
-			print_r($agent);
-			//die();
-		}
-		$res = preg_match("/^([A-Za-z0-9]{3,15}) .*<$/", $agent[0], $matches);
-		$agent = $matches[1];
-
-		$ap = preg_grep("/ AP$/", $lines);
-		$ap = array_values($ap);
-		if (sizeof($ap) != 1) { 
-			echo "error detecting ap, not 1 result: ";
-			print_r($ap);
-			//die();
-		}
-		$res = preg_match("/([0-9,]{1,10})\/", $ap[0], $matches);
-		$ap = preg_replace("/,/", "", $matches[1]);
-
-		$stats = array();
-
-		foreach(StatTracker::getStats() as $stat) {
-			$stats[$stat->stat] = self::findAgentStatValue($stat->name, $stat->unit, $lines);
-		}
-
-		// Add in AP, since it requires special processing
-		if (empty($stats['ap'])) {
-			//$stats['ap'] = $ap;
-		}
-
-		print_r(array("agent" => $agent, "stats" => $stats));
-	}
-
-	private static function findAgentStatValue($stat_name, $unit, &$lines) {
-		$score = array();
-		$index = -1;
-		print_r($lines);
-
-		foreach ($lines as $line) {
-			$index++;
-			// Skip lines that clearly aren't a match
-			if (strlen($stat_name) >= strlen($line))
-				continue;
-
-			// Strip numerics from the line for comparison
-			$name_line = trim(preg_replace("/[0-9,]/", "", $line));
-			$name_line = trim(preg_replace("/".$unit."$/i", "",  $name_line));
-			$value_line = trim(str_replace(explode(" ", $stat_name), "", $line));
-			$value_line = trim(str_replace(explode(" ", $unit), "", $value_line));
-
-			$s = array (
-				"index" => $index,
-				"name" => $stat_name,
-				"line" => $line,
-				"value_line" => $value_line,
-				"name_line" => $name_line,
-				"diff" => abs(strlen($stat_name) - strlen($name_line)),
-				"score" => levenshtein($stat_name, $name_line),
-				"threshold" => (strlen($stat_name) / 2),
-				"value" => trim(preg_replace("/[^0-9lt]/", "", $value_line)) // 1 is sometimes read as l or t
-			);
-
-			
-
-			// If more than half of the characters needs to be replaced, clearly not a match
-			if (($s['score'] + $s['diff']) >= $s['threshold']) {
-				echo "REJECT";
-				print_r($s);
-				continue;
+	 * @param string $imagePath the path of the image to convert.
+	 *
+	 * @return path to new PBM image
+	 */
+	public static function convertToPBM($imagePath, $deleteOriginal = false) {
+		$img = new Imagick();
+		$quantumRange = $img->getQuantumRange();
+		$black_point = $quantumRange['quantumRangeLong'] * 0.04;
+		$white_point = $quantumRange['quantumRangeLong'] - $black_point;
+		$img->readImage($imagePath);
+		$identify = $img->identifyImage();
+		$newFile = UPLOAD_DIR . self::getTempFileName("pbm");
+		$x = 0; $y = 0;
+		$pixel = $img->getImagePixelColor($x, $y);
+		$color = $pixel->getColorAsString();
+		if ($color == "srgb(0,0,0") {
+			for ( ; $x < 500 ; $x += 1 , $y += 1) {
+				$pixel = $img->getImagePixelColor($x, $y);
+				$color = $pixel->getColorAsString();
+				if (!preg_match('/^srgb\(\d{1,2},\d{1,2},\d{1,2}\)$/sxmi', $color)) {
+					break;
+				}
 			}
-
-			$s['final_value'] = $s['value'];
-
-			// replace l with 1
-			$s['value'] = str_replace("l", "1", $s['value']);
-			$s['value'] = str_replace("t", "1", $s['value']);
-
-			$score[] = $s;
+		}
+		else {
+			for ( ; $y < 500 ; $y += 5) {
+				$pixel = $img->getImagePixelColor($x, $y);
+				$color = $pixel->getColorAsString();
+				if ($color == 'srgb(0,0,0)') {
+					break;
+				}
+			}
+			for ( ; $x < 500 ; $x += 1 , $y += 1) {
+				$pixel = $img->getImagePixelColor($x, $y);
+				$color = $pixel->getColorAsString();
+				if (!preg_match('/^srgb\(\d{1,2},\d{1,2},\d{1,2}\)$/sxmi', $color)) {
+					break;
+				}
+			}
+			$count = 0;
+			for ( ; $x < 500 ; $x += 1 , $y -= 1) {
+				$count++;
+				$pixel = $img->getImagePixelColor($x, $y);
+				$color = $pixel->getColorAsString();
+				if (preg_match('/^srgb\((\d{1,2}),(\d{1,2}),(\d{1,2})\)$/sxmi', $color, $matches)
+					&& $matches[1] < 80
+					&& $matches[2] < 80
+					&& $matches[3] < 80) {
+					if ($count <= 2) {
+						$y += 1;
+						continue;
+					} else {
+						$x -= 1;
+						$y += 1;
+						break;
+					}
+				}
+			}
 		}
 
-		usort($score, function($a, $b) {
-			if ($a["score"] == $b["score"]) return 0;
-			return ($a["score"] < $b["score"]) ? -1 : 1;
-		});
-
-		// "Best match" should be first entry
-		$line = array_shift($score);
-		array_splice($lines, $line['index'], 1);
-
-		print_r($line);
-
-		if ($stat_name == "AP") {
-			$line['value'] = preg_replace("/[^0-9]/", "", explode(" ", $line['value_line'])[0]);
+		for ( ; $x < 500 ; $x += 1) {
+			$pixel = $img->getImagePixelColor($x, $y);
+			$color = $pixel->getColorAsString();
+			if (preg_match('/^srgb\((\d{1,2}),(\d{1,2}),(\d{1,2})\)$/sxmi', $color, $matches)
+				&& $matches[1] < 80
+				&& $matches[2] < 80
+				&& $matches[3] < 80) {
+				$x -= 1;
+				break;
+			}
+		}
+		for ( ; $x < 500 ; $x += 1 , $y += 1) {
+			$pixel = $img->getImagePixelColor($x, $y);
+			$color = $pixel->getColorAsString();
+			if (preg_match('/^srgb\(\d,\d,\d\)$/sxmi', $color)) {
+				$y += 1;
+				break;
+			}
+		}
+		$rightx = $x;
+		$x = 0;
+		for ( ; $x < 500 ; $x += 1) {
+			$pixel = $img->getImagePixelColor($x, $y);
+			$color = $pixel->getColorAsString();
+			if (!preg_match('/^srgb\(\d,\d,\d\)$/sxmi', $color)) {
+				break;
+			}
 		}
 
-		return $line['value'];
+		$space = $x;
+		$x += $rightx;
+		$y += $space;
+
+		if ($space <= 4) {
+			for ( ; $x < 500 ; $x += 1 , $y += 1) {
+				$pixel = $img->getImagePixelColor($x, $y);
+				$color = $pixel->getColorAsString();
+				if (!preg_match('/^srgb\(\d{1,2},\d{1,2},\d{1,2}\)$/sxmi', $color)) {
+					$x -= 1;
+					break;
+				}
+			}
+		}
+
+		for ( ; $y < 500 ; $y += 1) {
+			$pixel = $img->getImagePixelColor($x, $y);
+			$color = $pixel->getColorAsString();
+			if (!preg_match('/^srgb\(\d{1,2},\d{1,2},\d{1,2}\)$/sxmi', $color)) {
+				break;
+			}
+		}
+		for ( ; $y < 500 ; $y += 1) {
+			$pixel = $img->getImagePixelColor($x, $y);
+			$color = $pixel->getColorAsString();
+			if (preg_match('/^srgb\(\d{1,2},\d{1,2},\d{1,2}\)$/sxmi', $color)) {
+				$x -= round($space / 2);
+				break;
+			}
+		}
+
+		$img->cropImage($identify['geometry']['width'], $identify['geometry']['height'] - $y, 0, $y);
+		$draw = new ImagickDraw();
+		$draw->setFillColor('black');
+		$draw->rectangle(0, 0, $x, $y);
+		$img->drawImage($draw);
+		$img->levelImage($black_point, 1, $white_point);
+		$img->resizeImage($identify['geometry']['width'] * 2, 0,  imagick::FILTER_LANCZOS, 1);
+		$img->writeImage($newFile);
+
+		if ($deleteOriginal) {
+			unlink($imagePath);
+		}
+
+		return $newFile;
+	}
+
+	/**
+	 * Invokes external OCR tool
+	 *
+	 * @param string $imagePath path to the image to scan
+	 * @param boolean $deleteImage whether or not to delete the image after scanning
+	 *
+	 * @return array of lines read from the image
+	 */
+	public static function executeOCR($imagePath, $deleteImage = true) {
+		exec("/usr/bin/ocrad -i $imagePath", $lines);
+
+		if ($deleteImage) {
+			unlink($imagePath);
+		}
+
+		return $lines;
+	}
+
+	/**
+	 * Converts a source image to PBM format for use with the OCRAD scanner. Also blacks out agent avatar and
+	 * stat labels.
+	 *
+	 * Special thanks to Eu Ji (https://plus.google.com/115412883382513227875/about) of ingress-stats.org
+	 *
+	 * @param string $imagePath the path of the image to convert.
+	 *
+	 * @return path to new PBM image
+	 */
+	public static function processAgentData($lines) {
+		$step = 'start';
+		$elements = array();
+		foreach($lines as $line) {
+			if ($step == 'start' && preg_match('/^\s*([\d\s\|.egiloqt,]+)\s*AP\s*$/sxmi', $line, $values)) {
+				$step = 'ap';
+				array_push($elements, $values[1]);
+			} elseif ($step == 'ap' && preg_match('/^\s*Discovery\s*$/sxmi', $line)) {
+				$step = 'discovery';
+				$count = 0;
+			} elseif ($step == 'discovery' && preg_match('/^\s*Building\s*$/sxmi', $line)) {
+				// inject a 0 if only 2 stats, which means that the agent has 0 portals discovered
+				if ($count == 2) {
+					$temp = array_pop($elements);
+					array_push($elements, '0');
+					array_push($elements, $temp);
+				}
+				$step = 'building';
+			} elseif ($step == 'building' && preg_match('/^\s*Combat\s*$/sxmi', $line)) {
+				$step = 'combat';
+			} elseif ($step == 'combat' && preg_match('/^\s*Health\s*$/sxmi', $line)) {
+				$step = 'health';
+			} elseif ($step == 'health' && preg_match('/^\s*Defense\s*$/sxmi', $line)) {
+				$step = 'defense';
+			} elseif ($step == 'defense' && preg_match('/^\s*Missions\s*$/sxmi', $line)) {
+				$step = 'missions';
+			} elseif ($step == 'discovery' && preg_match('/^\s*([\d\s\|.aegiloqt,]+)\s*(?:XM)?\s*$/sxmi', $line, $values)) {
+				$count++;
+				array_push($elements, $values[1]);
+			} elseif ($step == 'building' && preg_match('/^\s*([\d\s\|.aegiloqt,]+)\s*(?:MUs|XM|km|kln)?\s*$/sxmi', $line, $values)) {
+				array_push($elements, $values[1]);
+			} elseif ($step == 'combat' && preg_match('/^\s*([\d\s\|.aegiloqt,]+)\s*$/sxmi', $line, $values)) {
+				array_push($elements, $values[1]);
+			} elseif ($step == 'health' && preg_match('/^\s*([\d\s\|.aegiloqt,]+)\s*(?:km|kln)\s*$/sxmi', $line, $values)) {
+				array_push($elements, $values[1]);
+			} elseif ($step == 'defense' && preg_match('/^\s*([\d\s\|.aegiloqt,]+)\s*(?:(?:(?:km|kln|MU)-)?(?:days|clays|ilays|cl_ys|__ys|d_ys|_ays|\(l_ys))\s*$/sxmi', $line, $values)) {
+				array_push($elements, $values[1]);
+			} elseif ($step == 'missions' && preg_match('/^\s*([\d\s\|.aegiloqt,]+)\s*$/sxmi', $line, $values)) {
+				array_push($elements, $values[1]);
+			} elseif (preg_match('/^\s*(month|week|now)\s*$/sxmi', $line, $values)) {
+				$warning = sprintf($lang['maybe because'], $values[1]);
+			}
+		}
+
+		$elements = preg_replace('/[.]|,|\s/', '', $elements);
+		$elements = preg_replace('/o/i', '0', $elements);
+		$elements = preg_replace('/\||l|i/i', '1', $elements);
+		$elements = preg_replace('/q/i', '4', $elements);
+		$elements = preg_replace('/t/i', '7', $elements);
+		$elements = preg_replace('/a|e/i', '8', $elements);
+		$elements = preg_replace('/g/', '9', $elements);
+
+		$data = array();
+		$stats = StatTracker::getStats();
+		$i = 0;
+
+		foreach ($stats as $stat) {
+			$data[$stat->stat] = $elements[$i++];
+		}
+
+		return $data;
 	}
 }
 ?>
