3865f7b4430959df5d196f309ad9090adb4c5d3f
John Luetke <john@johnluetke.net>
2014-11-11 15:19:51 -0800

    Adding database functions

---
+++ database/functions/GetInterceptForAgentAndStat.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+
+DROP FUNCTION IF EXISTS `GetInterceptForAgentAndStat` $$
+
+CREATE DEFINER=`admin`@`localhost` FUNCTION `GetInterceptForAgentAndStat`(`agent_name` VARCHAR(15), `stat_key` VARCHAR(20)) RETURNS int(10)
+    READS SQL DATA
+BEGIN
+
+RETURN GetInterceptForAgentAndStatBetweenDates(agent_name,
+                                               stat_key,
+                                               DATE_SUB(NOW(), INTERVAL 30 DAY),
+                                               NOW());
+
+END $$
+DELIMITER ;
