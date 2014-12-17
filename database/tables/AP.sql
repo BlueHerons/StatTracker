82f175bf18d101b5ae44bb2d4125faa09582cb8e
John Luetke <john@johnluetke.net>
2014-12-17 09:35:33 -0800

    Adding AP gain for mods_deployed

--- database/tables/AP.sql
+++ database/tables/AP.sql
@@ -14,15 +14,16 @@ CREATE TABLE `AP` (
 
 INSERT INTO `AP`
 (`stat`,               `grouping`, `sequence`, `ap_gain`, `factor`) VALUES
-('fields_created',     3,          1,          1250,      1.000),
 ('fields_destroyed',   1,          3,          750,       1.000),
-('links_created',      3,          2,          313,       1.000),
 ('links_destroyed',    1,          2,          187,       1.000),
-('portals_captured',   3,          3,          500,       1.000),
-('portals_discovered', 3,          0,          1000,      1.000),
-('res_deployed',       3,          4,          125,       0.000),
 ('res_destroyed',      1,          1,          75,        1.000),
-('xm_recharged',       3,          5,          10,        0.001);
+('portals_discovered', 3,          0,          1000,      1.000),
+('fields_created',     3,          1,          1250,      1.000),
+('links_created',      3,          2,          313,       1.000),
+('portals_captured',   3,          3,          500,       1.000),
+('mods_deployed',      3,          4,          125,       1.000),
+('res_deployed',       3,          5,          125,       0.000),
+('xm_recharged',       3,          6,          10,        0.001);
 
 ALTER TABLE `AP`
   ADD CONSTRAINT `AP_ibfk_1` FOREIGN KEY (`stat`) REFERENCES `Stats` (`stat`);
