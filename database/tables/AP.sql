14abc5a31214c6b8c94bace33fdc05d062b00f1f
John Luetke <john@johnluetke.net>
2015-01-15 09:49:00 -0800

    Update AP.sql
    Adding AP gained for deploying a mod

--- database/tables/AP.sql
+++ database/tables/AP.sql
@@ -18,11 +18,12 @@ INSERT INTO `AP`
 ('fields_destroyed',   1,          3,          750,       1.000),
 ('links_created',      3,          2,          313,       1.000),
 ('links_destroyed',    1,          2,          187,       1.000),
+('mods_deployed',      3,          4,          125,       1.000),
 ('portals_captured',   3,          3,          500,       1.000),
 ('portals_discovered', 3,          0,          1000,      1.000),
-('res_deployed',       3,          4,          125,       0.000),
+('res_deployed',       3,          5,          125,       0.000),
 ('res_destroyed',      1,          1,          75,        1.000),
-('xm_recharged',       3,          5,          10,        0.001);
+('xm_recharged',       3,          6,          10,        0.001);
 
 ALTER TABLE `AP`
   ADD CONSTRAINT `AP_ibfk_1` FOREIGN KEY (`stat`) REFERENCES `Stats` (`stat`);
