601906daf418ca1fad2fe4e2bdc5b45f0b5fdf8c
John Luetke <john@johnluetke.net>
2015-02-17 14:02:51 -0800

    Revert "Revert "Issue 6""

--- api.php
+++ api.php
@@ -61,7 +61,7 @@ $app->get("/api/{auth_code}/profile/{when}.{format}", function($auth_code, $when
 	
 	$t = new stdClass;
 
-	if (preg_match("/[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}/", $when)) {
+	if (StatTracker::isValidDate($when)) {
 		$ts = $agent->getUpdateTimestamp($when, true);
 
 		if ($ts == null) {
@@ -77,6 +77,9 @@ $app->get("/api/{auth_code}/profile/{when}.{format}", function($auth_code, $when
 		$response->date = date("c", $agent->getUpdateTimestamp());
 		$response->badges = $agent->getBadges();
 		$response->stats = $agent->getStats("latest", true);
+	}
+	else {
+		return $app->abort(404);
 	}
 
 	switch ($format) {
@@ -195,7 +198,7 @@ $app->post("/api/{auth_code}/submit", function($auth_code) use ($app) {
 	$response = StatTracker::handleAgentStatsPost($agent, $_POST);
 	$app['session']->set("agent", Agent::lookupAgentByAuthCode($auth_code));
 
-	return $response;
+	return $app->json($response);
 })->before($validateRequest);
 
 $app->post("/api/{auth_code}/ocr", function(Request $request, $auth_code) use ($app) {
