6832dce409941abab573c2ac368d6aa6f6dad6d4
johnluetke <johnl1479@gmail.com>
2015-06-02 22:23:00 +0000

    Calculate regression slope for days with no submission. Fixes #81

--- database/functions/GetTimepointForAgentAndDate.sql
+++ database/functions/GetTimepointForAgentAndDate.sql
@@ -2,16 +2,14 @@ DELIMITER $$
 
 DROP FUNCTION IF EXISTS `GetTimepointForAgentAndDate` $$
 
-CREATE FUNCTION `GetTimepointForAgentAndDate`(`agent_name` VARCHAR(20), `start_date` DATE) RETURNS int(4)
+CREATE FUNCTION `GetTimepointForAgentAndDate`(`agent_name` VARCHAR(15), `start_date` DATE) RETURNS int(4)
     READS SQL DATA
 BEGIN
 
-SET @first_submission = CURDATE();
-SET @timepoint = 0;
-
-
-SELECT MIN(date) FROM Data WHERE agent = agent_name INTO @first_submission;
-SELECT DATEDIFF(start_date, @first_submission) + 1 INTO @timepoint;
+SELECT COALESCE(
+    (SELECT timepoint FROM Data WHERE agent = agent_name and date = start_date LIMIT 1),
+    (SELECT DATEDIFF(start_date, MIN(date)) + 1 FROM Data WHERE agent = agent_name)
+) INTO @timepoint;
 
 RETURN @timepoint;
 
